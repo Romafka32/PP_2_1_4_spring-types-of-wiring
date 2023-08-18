fdcd2dabbdb3dd41789e605a4b6b6ab3928d87b3
Romafka32 <romankuznetsov0406@gmail.com>
2023-08-18 18:10:12 +0300

    Task completed

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    public Deth8() {
+    }
+
+    @Override
+    public String toString() {
+        return " А Иван-царевич киллеру и говорит:\"Ты меня не грузи, я расценки знаю. Запалчу нормально. Тут и сказке конец. Взял киллер предоплату и убил Кощея.\"";
+    }
 }
